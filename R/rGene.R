72b8c924d8b2f348537ae8ee575bee26371be2b8
NolanBentley <nolanbentley@gmail.com>
2021-10-17 18:57:01 -0500

    added get commands to load data sets

--- R/rGene.R
+++ R/rGene.R
@@ -1,36 +1,46 @@
 #' Random gene selector
 #'
-#' @param id a length 1 character vector of your ID (should match a value in biol321Ids)
-#' @param seed a length 1 numeric vector used to set pseudo-random assignments. This seed set per semester; do not change unless instructed to do so.
-#' @param randomRow a length one logical vector. Only change this value if your Id is not in the biol321Ids object for some reason.
-#' @return
-#' @export
+#' @param id A length 1 character vector of your ID (should match a value in biol321Ids)
+#' @param seed A length 1 numeric vector used to set pseudo-random assignments. This seed set per semester; do not change unless instructed to do so.
+#' @param idTable A table of course IDs created 1 per semester. Do not change.
+#' @param df A table of reviewed human genes downloaded from uniprot. Do not change.
+#' @param randomRow A length one logical vector. Only change this value if your Id is not in the biol321Ids object for some reason.
 #'
+#' @return The random humanGene row that was selected based on your ID
+#' @export
 #' @examples
-#' assignedGene <- rGene("nbb624")
-#' print(assignedGene[,1:4])
+#' # Typical use
+#' rGene("nbb624")
 
-rGene <- function(id, seed = 20211017, randomRow = F) {
+rGene <- function(id,
+                  seed = 20211017,
+                  randomRow = F,
+                  idTable = get("biol321Ids"),
+                  df      = get("humanGenes")
+                  ) {
     #Load data
-    utils::data("biol321Ids", envir = environment())
-    utils::data("humanGenes", envir = environment())
+    #utils::data("biol321Ids", envir = environment())
+    #utils::data("humanGenes", envir = environment())
+    #idTable <- get("biol321Ids")
+    #df      <- get("humanGenes")
+
     #Find matching ID
-    row <- match(id, biol321Ids[, 1])
+    row <- match(id, idTable[, 1])
     #Overide match if randomRow
     if (randomRow) {
-        row <- sample(1:nrow(humanGenes),1)
+        row <- sample(1:nrow(df),1)
     }
     #Check row
     if (is.na(row)) {
-        stop("Id not found in object. If this was expected, change randomRow to T")
+        stop("Id not found in biol321Ids. If this was expected (it probably wasn't), change randomRow to T.")
     }
 
     if (!is.numeric(seed) |
         length(seed) != 1) {
         stop("seed needs to be a length 1 numeric vector")
     }
-    #Set seed and
+    #Set seed and isolate current row
     set.seed(seed)
-    currRow <- sample(1:nrow(humanGenes), size = nrow(humanGenes), replace = F)[row]
-    return(humanGenes[currRow,])
+    currRow <- sample(1:nrow(df), size = nrow(df), replace = F)[row]
+    return(df[currRow,])
 }
