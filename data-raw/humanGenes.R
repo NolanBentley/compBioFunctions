d063ba8ede280181d7ca62bb162bfea7b31ddd40
NolanBentley <nolanbentley@gmail.com>
2021-10-18 07:49:30 -0500

    Subset humanGenes to rows 200<=len<=2000 AA.

--- data-raw/humanGenes.R
+++ data-raw/humanGenes.R
@@ -1,3 +1,8 @@
 ## code to prepare `humanGenes` dataset goes here
-humanGenes <- openxlsx::read.xlsx("data-raw/raw/uniprotReviewedHumanGenes.xlsx")
+humanGenes <- openxlsx::read.xlsx("data-raw/raw/uniprotReviewedHumanGenes.xlsx",sheet = 1)
+attributes(humanGenes)$minLen <- 200
+attributes(humanGenes)$maxLen <- 2000
+humanGenes <- humanGenes[humanGenes$Length >= attributes(humanGenes)$minLen, ]
+humanGenes <- humanGenes[humanGenes$Length <= attributes(humanGenes)$maxLen, ]
+
 usethis::use_data(humanGenes, overwrite = F)
