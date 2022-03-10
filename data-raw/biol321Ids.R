9b1dd284c95b056c1738af38be08c3ce92b7967d
Nolan Bentley <nolanbentley@gmail.com>
2022-03-10 08:25:01 -0600

    Updated documentation for Spring 2022

--- data-raw/biol321Ids.R
+++ data-raw/biol321Ids.R
@@ -1,3 +1,3 @@
 ## code to prepare `biol321Ids` dataset goes here
-
-usethis::use_data(biol321Ids, overwrite = TRUE)
+biol321Ids <- read.csv(file = "data-raw/raw/idsSpring2022.csv", stringsAsFactors = F)
+usethis::use_data(biol321Ids, overwrite = T)
