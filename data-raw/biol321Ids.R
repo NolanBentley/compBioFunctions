b53d8f7d93a817351161431b29b47a982d6000d1
Nolan Bentley <nolanbentley@gmail.com>
2022-10-24 15:40:01 -0500

    Updated the package for Fall 2022 Wk10Hw
    - Added in new id list
    - Modified the data-raw script
    - Reran the package creations script, verified no significant errors, and committed this message.

--- data-raw/biol321Ids.R
+++ data-raw/biol321Ids.R
@@ -1,3 +1,3 @@
 ## code to prepare `biol321Ids` dataset goes here
-biol321Ids <- read.csv(file = "data-raw/raw/idsSpring2022.csv", stringsAsFactors = F)
+biol321Ids <- read.csv(file = "data-raw/raw/idsFall2022.csv", stringsAsFactors = F)
 usethis::use_data(biol321Ids, overwrite = T)
